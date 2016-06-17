ad3cce57f1cb18489da4127a5602cc9de1d3490e
rheza hidajat <r.hidajat@yahoo.com>
2016-06-17 15:11:56 -0700

    add addition testing

--- src/main/java/com/rheezy/practice/largenumber/TestBigInteger.java
+++ src/main/java/com/rheezy/practice/largenumber/TestBigInteger.java
@@ -3,11 +3,24 @@ package com.rheezy.practice.largenumber;
 import java.math.BigInteger;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class TestBigInteger {
 	
 	@Test
 	public void testCreating() {
 		BigInteger reallyBigNumber = new BigInteger("1234567890987654321");
+		Assert.assertNotNull(reallyBigNumber);
 	}
+	
+	@Test
+	public void testAddition() {
+		BigInteger reallyBigNumber = new BigInteger("1234567890987654321");
+		BigInteger notAsBigNumber = new BigInteger("12345678");
+		BigInteger additionResult = reallyBigNumber.add(notAsBigNumber);
+		
+		Assert.assertEquals(new BigInteger("1234567890999999999"), additionResult);
+	}
+	
+	
 }
