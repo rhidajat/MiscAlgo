721e68c548a88eeefe8a3bd741cd1cf4b821748a
rheza hidajat <r.hidajat@yahoo.com>
2016-06-17 14:54:49 -0700

    initial commit

---
+++ src/main/java/com/rheezy/practice/largenumber/TestBigInteger.java
@@ -0,0 +1,13 @@
+package com.rheezy.practice.largenumber;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+public class TestBigInteger {
+	
+	@Test
+	public void testCreating() {
+		BigInteger reallyBigNumber = new BigInteger("1234567890987654321");
+	}
+}
